4cc171acf270c36d7d6159a562f50600132a5ead
Timothy Shee <timothy_shee@hotmail.com>
2020-10-30 18:06:02 -0400

    Initialized multi-lang i18n app structure
    - Added pages
    - Added locale constants
    - Changed App.js & index.js
    - Added i18n.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
+import React from 'react'
+// Translation with i18n
+import { withTranslation } from 'react-i18next'
+// Routing
+import { HashRouter, Route } from "react-router-dom";
+// Custom menu: nav and sidebar
+import NavigationMenu from "./components/NavigationMenu"
+// Pages
+import HomePage from './pages/HomePage';
+import BrowsePage from './pages/BrowsePage';
+import HistoryDashboard from './pages/HistoryPage'
+// Sidebar icons
+import HomeIcon from '@material-ui/icons/Home'
+import PageviewIcon from '@material-ui/icons/Pageview';
+import HistoryIcon from '@material-ui/icons/History';
+// load icons here
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+function App(props) {
+  // custom hook to call i18n changeLanguage function
+  const { t } = props;
+  const langCallback = (event) => {
+    let newLang = event.target.value;
+    props.i18n.changeLanguage(newLang)
+  }
+
+  const navMenuRouteData = [
+    { text: t('navMenu.HOME'), link: '/', iconFunc: () => { return <HomeIcon /> } },
+    { text: t('navMenu.BROWSE'), link: "/browse", iconFunc: () => { return <PageviewIcon /> } },
+    { text: t('navMenu.HISTORY'), link: "/history", iconFunc: () => { return <HistoryIcon /> } },
+  ]
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <HashRouter initialIndex={0}>
+      <div style={{backgroundColor: "#DAE3E7"}}>
+        <NavigationMenu
+          routeData={navMenuRouteData}
+          logo={logo}
+          langCallback={langCallback}
+          registerText={t('navMenu.register')}
+          signInText={t('navMenu.signIn')}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Route exact path="/" render={(props) => <HomePage {...props} t={t} /> } />
+          <Route path="/browse" render={(props) => <BrowsePage {...props} t={t} /> } />
+          <Route path="/history" render={(props) => <HistoryDashboard {...props} t={t} /> } />
+        </NavigationMenu>
+      </div>
+    </HashRouter>
   );
 }
 
-export default App;
+export default withTranslation()(App);
