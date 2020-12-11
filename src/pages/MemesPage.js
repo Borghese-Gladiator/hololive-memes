e6791f813728c295d06302a0a57c8c9c50c7d0c4
Timothy Shee <timothy_shee@hotmail.com>
2020-12-11 01:24:53 -0500

    Update MemesPage.js

--- src/pages/MemesPage.js
+++ src/pages/MemesPage.js
@@ -3,12 +3,9 @@ import Container from '@material-ui/core/Container'
 import MemeCardList from '../components/MemeCardList';
 import { memePosts } from '../constants/memeConstants';
 
-export default function MemesPage(props) {
-  const { t } = props
-
+export default function MemesPage() {
   return (
     <Container>
-      <h2 style={{textAlign: "center"}}>{t("animemes.title")}</h2>
       <MemeCardList memeData={memePosts} />
     </Container>
   )
