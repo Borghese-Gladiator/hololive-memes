9f096dfdc0c76da05400b838b88dd1addcaa1b81
Timothy Shee <timothy_shee@hotmail.com>
2020-12-11 02:18:12 -0500

    Fix up pagination & enable users to select # memes per page

--- src/components/MemeCardList/MemeCardList.js
+++ src/components/MemeCardList/MemeCardList.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import clsx from 'clsx';
 import { makeStyles } from '@material-ui/core/styles';
-// AboutRoles Container
+// Material UI components
 import Container from '@material-ui/core/Container'
 import Grid from '@material-ui/core/Grid';
 import Pagination from '@material-ui/lab/Pagination';
+// top right form
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
 // Custom component import
 import MemeCard from './MemeCard'
 
@@ -14,10 +18,19 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(5),
     '& > *': {
       marginTop: theme.spacing(2),
-    }
+    },
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 130,
   },
   control: {
     padding: theme.spacing(2),
+  },
+  rightColumnFlex: {
+    display: 'flex',
+    justifyContent: 'flex-end',
+    alignItems: 'flex-end'
   },
   columnFlex: {
     display: 'flex',
@@ -30,28 +43,49 @@ const useStyles = makeStyles((theme) => ({
 export default function MemeCardList(props) {
   const classes = useStyles();
   const { id, memeData } = props;
-  const numberOfItemsForPage = 30;
-  const numPages = Math.ceil(memeData.length / numberOfItemsForPage);
-  console.log(memeData.length);
-
+  // initialize pagination constants
+  const [numItemsPerPage, setNumItemsPerPage] = React.useState(30);
   const [pageNumber, setPageNumber] = React.useState(1);
-  const handleChange = (event, value) => {
+  const numPages = Math.ceil(memeData.length / numItemsPerPage);
+  const handlePageChange = (event, value) => {
     setPageNumber(value);
   };
-
+  const handleSelectPage = (event) => {
+    setNumItemsPerPage(event.target.value);
+  };
+  
   return (
-    <Container id={id} className={clsx(classes.root, classes.columnFlex)}>
-      <Grid container justify="center" spacing={1}>
-        {memeData.slice(((pageNumber - 1)*(numberOfItemsForPage)), ((pageNumber)*(numberOfItemsForPage))).map((obj, idx) => (
-          <Grid key={`${obj.name} ${idx}`} item xs={12} sm={6} lg={4} xl={3}>
-            <MemeCard
-              path={obj.imgPath}
-              name={obj.title}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <Pagination count={numPages} page={pageNumber} onChange={handleChange} color="primary" size="large" showFirstButton showLastButton />
+    <Container id={id} className={classes.root}>
+      <div className={classes.rightColumnFlex}>
+        <FormControl className={classes.formControl}>
+          <InputLabel id="demo-simple-select-label">Memes per Page</InputLabel>
+          <Select
+            labelId="demo-simple-select-label"
+            id="demo-simple-select"
+            value={numItemsPerPage}
+            onChange={handleSelectPage}
+          >
+            <MenuItem value={10}>10</MenuItem>
+            <MenuItem value={20}>20</MenuItem>
+            <MenuItem value={30}>30</MenuItem>
+            <MenuItem value={40}>40</MenuItem>
+            <MenuItem value={50}>50</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
+      <div className={classes.columnFlex}>
+        <Grid container justify="center" spacing={1}>
+          {memeData.slice(((pageNumber - 1)*(numItemsPerPage)), ((pageNumber)*(numItemsPerPage))).map((obj, idx) => (
+            <Grid key={`${obj.name} ${idx}`} item xs={12} sm={6} lg={4} xl={3}>
+              <MemeCard
+                path={obj.imgPath}
+                name={obj.title}
+              />
+            </Grid>
+          ))}
+        </Grid>
+        <Pagination count={numPages} page={pageNumber} onChange={handlePageChange} color="primary" size="large" showFirstButton showLastButton />
+      </div>
     </Container>
   );
 }
