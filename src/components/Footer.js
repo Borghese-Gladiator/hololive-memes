2b6b45d37a96af4a10eb2a681997116af1cfd39d
Timothy Shee <timothy_shee@hotmail.com>
2020-12-11 01:35:30 -0500

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,8 +3,6 @@ import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import Link from '@material-ui/core/Link';
-import Grid from '@material-ui/core/Grid';
-import Container from '@material-ui/core/Container';
 // List components
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
