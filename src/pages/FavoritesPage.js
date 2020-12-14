22c810ee1d9368c3b25e2af35b3cd2f56df88a41
Timothy Shee <timothy_shee@hotmail.com>
2020-12-14 18:35:40 -0500

    Write Favorites & History pages

--- src/pages/FavoritesPage.js
+++ src/pages/FavoritesPage.js
@@ -1,10 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
+// custom components
+import MemeCardList from '../components/MemeCardList';
 
-export default function FavoritesPage() {
+export default function HistoryPage() {
+  const [memeList, setMemeList] = useState([]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // add to localStorage history
+    const favoriteListStr = localStorage.getItem('history');
+    if (favoriteListStr) {
+      setMemeList(JSON.parse(favoriteListStr));
+    }
+  }, []);
+
   return (
     <Container>
       <h1 style={{textAlign: "center"}}>Favorites</h1>
+      {
+        memeList && memeList.length > 0
+          ? <MemeCardList memeData={memeList} />
+          : <Container>
+              <h4>No memes in history</h4>
+            </Container>
+      }
     </Container>
   )
 }
--- src/pages/HistoryPage.js
+++ src/pages/HistoryPage.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import Container from '@material-ui/core/Container';
+// custom components
+import MemeCardList from '../components/MemeCardList';
 
 export default function HistoryPage() {
+  const [memeList, setMemeList] = useState([]);
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    // add to localStorage history
+    const historyListStr = localStorage.getItem('history');
+    if (historyListStr) {
+      const historyList = JSON.parse(historyListStr);
+      setMemeList(historyList);
+    }
+  }, []);
+
   return (
     <Container>
       <h1 style={{textAlign: "center"}}>History</h1>
+      {
+        memeList && memeList.length > 0
+          ? <MemeCardList memeData={memeList} />
+          : <Container>
+              <h4>No memes in history</h4>
+            </Container>
+      }
     </Container>
   )
 }
