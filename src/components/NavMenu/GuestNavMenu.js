5f2d79475c47a4846c02659e61656fa93880c69c
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2020-12-18 07:37:48 -0800

    Add responsive (Major refactor)

--- src/components/NavMenu/GuestNavMenu.js
+++ src/components/NavMenu/GuestNavMenu.js
@@ -4,17 +4,24 @@ import { makeStyles } from '@material-ui/core/styles';
 import CustomLink from '../CustomLink';
 // custom component
 import LanguageSelector from './LanguageSelector';
-// appbar Material UI components
-import { AppBar, Button, CssBaseline, Toolbar } from '@material-ui/core';
-// dropdown button
-import Menu from '@material-ui/core/Menu';
+// Material UI components
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
 import MenuItem from '@material-ui/core/MenuItem';
-// instruction Material UI components
+import Menu from '@material-ui/core/Menu';
+import MoreIcon from '@material-ui/icons/MoreVert';
+
+
+import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+
+// instruction Material UI components
 import Container from '@material-ui/core/Container';
 import Backdrop from '@material-ui/core/Backdrop';
 import Paper from '@material-ui/core/Paper';
-// icons
+
+import HelpIcon from '@material-ui/icons/Help';
 import DetailsIcon from '@material-ui/icons/Details';
 import ChangeHistoryIcon from '@material-ui/icons/ChangeHistory';
 
@@ -29,16 +36,28 @@ const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
   logo: {
     height: 50,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
   },
   sectionDesktop: {
     display: 'none',
     [theme.breakpoints.up('md')]: {
-      display: 'flex'
+      display: 'flex',
     },
   },
-  appBarSpacer: theme.mixins.toolbar,
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
   backdrop: {
     zIndex: theme.zIndex.drawer + 1,
   },
@@ -49,91 +68,140 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center'
   }
 }));
-    
 
 export default function GuestNavMenu(props) {
   const classes = useStyles();
   const { logo, langCallback } = props;
-
-  // how to use instructions
-  const [open, setOpen] = React.useState(false);
-  const handleClose = () => {
-    setOpen(false);
-  };
-  const handleToggle = () => {
-    setOpen(!open);
-    console.log(open);
-  };
-
-  
   const [anchorEl, setAnchorEl] = React.useState(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = React.useState(null);
+  const [backdropOpen, setBackdropOpen] = React.useState(false);
 
-  const handleClick = (event) => {
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleProfileMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
-  const handleDropdownClose = () => {
-    setAnchorEl(null);
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
   };
 
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
+
+  const handleMobileMenuOpen = (event) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
+
+  const menuId = 'primary-search-account-menu';
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      
+      <MenuItem onClick={handleMenuClose}>
+        <CustomLink ariaLabel={`Link to history page`} to={'/history'}>
+          History
+        </CustomLink>
+      </MenuItem>
+      <MenuItem onClick={handleMenuClose}>
+        <CustomLink ariaLabel={`Link to favorites page`} to={'/favorites'}>
+          Favorites
+        </CustomLink>
+      </MenuItem>
+    </Menu>
+  );
+
+  const mobileMenuId = 'primary-search-account-menu-mobile';
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem onClick={handleProfileMenuOpen}>
+        <IconButton
+          aria-label="saved menu routing"
+          aria-controls="primary-search-saved-menu"
+          aria-haspopup="true"
+          color="inherit"
+        >
+          { anchorEl ? <ChangeHistoryIcon /> : <DetailsIcon /> }
+        </IconButton>
+        <p>Open Saved</p>
+      </MenuItem>
+    </Menu>
+  );
+
   return (
-    <div className={classes.root}>
-      <CssBaseline />
-      <AppBar
-        position="absolute"
-        className={classes.appBar}
-      >
-        <Toolbar variant="dense">
+    <div className={classes.grow}>
+      <AppBar position="static" className={classes.appBar}>
+        <Toolbar>
           <CustomLink to={"/"}>
-            <Button 
-              aria-label={"back to home image"}
-              className={classes.toolbarIcon}
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
             >
-              <Typography variant="button" display="block" gutterBottom>
-                {""}
-              </Typography>
               <img src={logo} className={classes.logo} alt="logo" />
-            </Button>
+            </IconButton>
           </CustomLink>
           <div className={classes.grow} />
           <div className={classes.sectionDesktop}>
-            <Button onClick={handleToggle} style={{color: 'inherit'}}>
-              <Typography variant="button" gutterBottom>
-                How to Use
-              </Typography>
-            </Button>
-            <Button aria-controls="simple-menu" aria-haspopup="true" onClick={handleClick} style={{color: 'inherit'}}>
+            <IconButton
+              aria-label="saved menu routing"
+              aria-controls="primary-search-saved-menu"
+              aria-haspopup="true"
+              color="inherit"
+              onClick={() => setBackdropOpen(!backdropOpen)}
+            >
+              <HelpIcon />
+            </IconButton>
+            <Button
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={handleProfileMenuOpen}
+              style={{color: 'inherit'}}
+            >
               <Typography variant="button" display="block" gutterBottom>
                 Open Saved
               </Typography>
               { anchorEl ? <ChangeHistoryIcon /> : <DetailsIcon /> }
             </Button>
-            <Menu
-              id="simple-menu"
-              anchorEl={anchorEl}
-              open={Boolean(anchorEl)}
-              onClose={handleDropdownClose}
-            >
-              <MenuItem onClick={handleDropdownClose}>
-                <CustomLink ariaLabel={`Link to history page`} to={'/history'}>
-                  History
-                </CustomLink>
-              </MenuItem>
-              <MenuItem onClick={handleDropdownClose}>
-                <CustomLink ariaLabel={`Link to favorites page`} to={'/favorites'}>
-                  Favorites
-                </CustomLink>
-              </MenuItem>
-            </Menu>
             <LanguageSelector onSelectLanguage={langCallback} />
+          </div>
+          <div className={classes.sectionMobile}>
+            <IconButton
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              color="inherit"
+            >
+              <MoreIcon />
+            </IconButton>
           </div>
         </Toolbar>
       </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
       <main>
-        <div className={classes.appBarSpacer} />
         {props.children}
-        {open ? (
-          <Backdrop className={classes.backdrop} open={open} onClick={handleClose}>
+        {backdropOpen ? (
+          <Backdrop className={classes.backdrop} open={backdropOpen} onClick={() => setBackdropOpen(false)}>
             <Container>
               <Paper className={classes.columnFlex} style={{
                 borderStyle: 'solid',                        
