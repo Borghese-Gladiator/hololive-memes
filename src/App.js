b150fb70d0888bd322ebbed24412f0df2d1e2022
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2020-12-18 05:40:24 -0800

    remove footer

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import { withTranslation } from 'react-i18next'; // Translation with i18n
 import { HashRouter, Route } from "react-router-dom"; // client-side routing
 // Custom components
 import NavMenu from './components/NavMenu/GuestNavMenu';
-import Footer from './components/Footer'
+// import Footer from './components/Footer'
+
 // pages
 import HomePage from './pages/HomePage';
 import MemesPage from './pages/memes/MemesPage';
 import HistoryPage from './pages/HistoryPage';
 import FavoritesPage from './pages/FavoritesPage';
 
-// footer icons
-import MailIcon from '@material-ui/icons/Mail';
-import RoomIcon from '@material-ui/icons/Room';
 // logo
 import logo from './constants/img/white_holomemes_logo.png';
 
+// footer icons
+// import MailIcon from '@material-ui/icons/Mail';
+// import RoomIcon from '@material-ui/icons/Room';
+/*
 const footerContactData = [
   {
     label: "+timtimysss@hotmail.com",
@@ -26,6 +28,7 @@ const footerContactData = [
     icon: () => { return <RoomIcon style={{fontSize: 30}} /> },
   },
 ];
+*/
 
 function App(props) {
   // custom hook to call i18n changeLanguage function
@@ -42,8 +45,7 @@ function App(props) {
           <Route exact path="/" render={(props) => <HomePage {...props} t={t} /> } />
           <Route path="/memes" render={(props) => <MemesPage {...props} t={t} /> } />
           <Route path="/history" render={(props) => <HistoryPage {...props} t={t} /> } />
-          <Route path="/favorites" render={(props) => <FavoritesPage {...props} t={t} /> } />
-          <Footer iconLabelList={footerContactData} />
+          <Route path="/favorites" render={(props) => <FavoritesPage {...props} t={t} /> } />          
         </NavMenu>
       </div>
     </HashRouter>
