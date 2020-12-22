e882e58e945f4cf44964d8bceed09681aa7ea1aa
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2020-12-22 04:24:15 -0500

    Major Refactor for clean code & fix bugs (

--- src/pages/memes/MemesPage.js
+++ src/pages/memes/MemesPage.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Switch,
   Route,
@@ -7,37 +7,83 @@ import {
 // child pages
 import MemeCardList from '../../components/MemeCardList';
 import MemeDetailsPage from './MemeDetailsPage';
-// custom component
-import MemeTagFilter from '../../components/MemeTagFilter';
-import MemeAutoComplete from '../../components/MemeAutoComplete';
 // load meme data
 import { memePosts } from '../../constants/memeConstants';
+// Material UI components
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from "@material-ui/core/TextField";
+import Button from '@material-ui/core/Button';
+import Autocomplete from "@material-ui/lab/Autocomplete";
+
+const useStyles = makeStyles((theme) => ({
+  autocomplete: {
+    width: 500,
+    margin: theme.spacing(1),
+  },
+  flexColCenter: {
+    display: 'flex',  
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  flexRowCenter: {
+    display: 'flex',  
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    '& > *': { // add padding between elements
+      margin: 5
+    },
+  }
+}));
+
+const FILTER_MAP = {
+  All: () => true,
+  Smug: meme => meme.tags.includes('smug'),
+  Angry: meme => meme.tags.includes('angry'),
+  Sleepy: meme => meme.tags.includes('sleepy'),
+  Panic: meme => meme.tags.includes('panic'),
+  Happy: meme => meme.tags.includes('happy'),
+};
+const FILTER_NAMES = Object.keys(FILTER_MAP);
 
 export default function MemesPage() {
   let match = useRouteMatch();
   
-  const [memeList, setMemeList] = React.useState(memePosts);
-  function filterByTagCallback(tagList) {
-    const filteredMemeList = memePosts.filter((val, idx) => {
-      let noTagInMeme = false;
-      for (const tag of tagList) {
-        if (!val.tags.includes(tag)) {
-          noTagInMeme = true;
-          break;
-        }
-      };
-      return noTagInMeme;
-    });
-    setMemeList(filteredMemeList);
-  }
+  const classes = useStyles();
+  const [filter, setFilter] = useState("All");
+  const [IDListFilter, setIDListFilter] = useState("");
 
-  function filterByIDCallback(id) {
-    const meme = memePosts.find((val) => val.id === id);
-    setMemeList([meme]);
-  }
-  
-  function removeFilterCallback() {
-    setMemeList(memePosts);
+  const filterList = FILTER_NAMES.map(name => (
+    name === filter
+      ?
+      <Button variant="contained" key={`${name}`} onClick={() => setFilter(name)} color="primary"style={{textDecoration: 'underline'}}>
+        #{name}
+      </Button>
+      :
+      <Button variant="contained" key={`${name}`} onClick={() => setFilter(name)}>
+        #{name}
+      </Button>
+  ));
+
+  const filteredMemeList = memePosts
+  .filter(FILTER_MAP[filter])
+  .filter(meme => {
+    if (IDListFilter === "") {
+      return true;
+    } else {
+      return IDListFilter.includes(meme.id)
+    }
+  });
+
+  const handleAutcompleteChange = (event, newValue) => {
+    // newValue is array of values that fit autocomplete // console.log(JSON.stringify(newValue, null, ' '));
+    if (newValue && newValue.length > 0) {
+      const idArray = newValue.map(val => val.id);
+      setIDListFilter(idArray);
+    } else {
+      setIDListFilter(""); // default value
+    }
   }
 
   return (
@@ -46,19 +92,30 @@ export default function MemesPage() {
         <MemeDetailsPage memeData={memePosts} />
       </Route>
       <Route path={match.path}>
-        <MemeAutoComplete memeData={memePosts} filterByIDCallback={filterByIDCallback} removeFilterCallback={removeFilterCallback} />
-        <MemeTagFilter filterByTagCallback={filterByTagCallback} removeFilterCallback={removeFilterCallback} />
-        <MemeCardList memeData={memeList} />
+        <div className={classes.flexColCenter}>
+          <Autocomplete
+            id="autocomplete"
+            className={classes.autocomplete}
+            multiple
+            options={memePosts}
+            getOptionLabel={option => option.title}
+            onChange={handleAutcompleteChange}
+            renderInput={params => (
+              <TextField
+                {...params}
+                variant="outlined"
+                label="Search Memes"
+                margin="normal"
+                fullWidth
+              />
+            )}
+          />
+          <div className={classes.flexRowCenter}>
+            {filterList}
+          </div>
+          <MemeCardList memeData={filteredMemeList} />
+        </div>
       </Route>
     </Switch>
   )
 }
-/*
-
-  function filterByTitleCallback(filterTitle) {
-    const filteredMemeList = memePosts.filter((val, idx) => {
-      return val.title === filterTitle;
-    });
-    setMemeList(filteredMemeList);
-  }
-*/
